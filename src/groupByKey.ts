c8ee21914ad215a86735366f936e0ffc9cfea873
eduferrs <edu.ferr96@gmail.com>
2026-05-08 19:30:37 -0300

    fix

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,11 +1,11 @@
 /**
- * GroupsMap<T> = { ... } 
+ * GroupsMap<T> = { ... }
  *   Criando um tipo genério para objetos ===
  *   Não tem uma formatação exata de quais são as propriedades que terá
- * 
+ *
  * [key: string]
  *    Está definindo que aceita apenas strings como nome das chaves/propriedades
- * 
+ *
  * : T[]
  *    Definindo que o que cada chave guarda é uma lista genérica
  */
@@ -13,18 +13,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-
-
 /**
  * items é a entrada. É uma lista de objetos (genéricos; do "tipo" T)
- * 
- * K é o "tipo" da chave. 
- * Fazer extends keyof T, vai funcionar como uma restrição 
- * O TS irá impedir que se tente agrupar por uma chave que não exista (no tipo genérico T)
- * 
+ *
+ * K é o "tipo" da chave.
+ * Fazer extends keyof T, vai funcionar como uma restrição
+ * O TS irá impedir que se tente agrupar por uma chave que não exista (no tipo
+ * genérico T)
+ *
  * E a função irá retornar um objeto genérico GroupsMap
- * Onde os VALORES da chave passada como argumento (key, uma chave existente nos obj de 
- * items) viram chaves no GroupsMap retornado
+ * Onde os VALORES da chave passada como argumento (key, uma chave existente
+ * nos obj de items) viram chaves no GroupsMap retornado
  */
 export function groupByKey<T, K extends keyof T>(
   items: T[],
@@ -33,10 +32,13 @@ export function groupByKey<T, K extends keyof T>(
   const groups: GroupsMap<T> = {};
 
   for (const item of items) {
-    // Pega o valor da propriedade e convertendo em string para usar como chave do objeto GroupsMap
-    // Ex.: se items é uma lista de objetos no formato { id: 1, color: 'red', country: 'Ukraine' }
+    // Pega o valor da propriedade e convertendo em string para usar como
+    // chave do objeto GroupsMap
+
+    // Ex.: se items é uma lista de objetos no formato
+    //    { id: 1, color: 'red', country: 'Ukraine' }
     // E key === color, 'red' irá virar uma chave no GroupsMap retornado
-  
+
     const groupValue = String(item[key]);
 
     // Se o grupo ainda não existir, inicia um array vazio
